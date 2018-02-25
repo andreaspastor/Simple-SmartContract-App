189158c30de3a65d4f2924997158f5357802fab6
Andréas PASTOR <andreas.pastor@etu.univ-nantes.fr>
2018-02-25 12:29:10 +0100

    correction contract + add UI

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.18;
 
 contract Voting {
 
@@ -15,12 +15,28 @@ contract Voting {
     mapping (uint => address) public voteToOwner;
     mapping (address => uint) ownerVoteCount;
 
-    function createVote(string _question) external {
+    function createVote(string _question) external returns (bool) {
         uint voteId = votes.push(Vote(_question, 0, 0)) - 1;
 
         voteToOwner[voteId] = msg.sender;
         ownerVoteCount[msg.sender]++;
-        VoteAdded(voteId, _question);
+        //VoteAdded(voteId, _question);
+        return true;
+    }
+
+    function getNbVoteId() external view returns(uint) {
+        return votes.length;
+    }
+
+    function getVote(uint _voteId) external view returns(string, uint, uint) {
+        require(votes.length > _voteId);
+        return (votes[_voteId].question,
+                votes[_voteId].countYes,
+                votes[_voteId].countNo);
+    }
+
+    function getVoteOwner(uint _voteId) external view returns(address) {
+        return voteToOwner[_voteId];
     }
 
 }
